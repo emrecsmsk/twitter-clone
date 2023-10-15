3d9f38bf9b68c45eb2e7498a66d195d1bda44f93
Emre Can Şimşek <emrecan.midyat@gmail.com>
2023-10-15 23:10:04 +0300

    set sidebar

--- src/layout/sidebar/index.jsx
+++ src/layout/sidebar/index.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Logo from './logo'
+import Menu from './menu'
 
 const Sidebar = () => {
     return (
         <div className="w-[275px] min-h-screen px-2">
-            Sidebar
+            <Logo />
+            <Menu />
         </div>
     )
 }
